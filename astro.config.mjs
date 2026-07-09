be0f9c81328e5f18828382ffd4f247344fe2a46f
Cedmpi <cedced@protonmail.com>
2026-07-09 12:30:12 +0200

    feat: add search

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,20 @@ import { unified } from "@astrojs/markdown-remark";
 
 import tailwindcss from "@tailwindcss/vite";
 import rehypeShortHeading from "./src/plugins/rehype-short-heading.mjs";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   i18n: {
-    locales: ["en","de"],
+    locales: ["en", "de"],
     defaultLocale: "en",
   },
-
   markdown: {
     processor: unified({
       rehypePlugins: [rehypeShortHeading],
     }),
   },
-
+  integrations: [pagefind()],
   vite: {
     plugins: [tailwindcss()],
   },
